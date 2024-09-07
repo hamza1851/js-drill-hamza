0987a7e7cbe887920a9476b62cc91ae0175c9fef
hamza1851 <hamza.dev011@gmail.com>
2024-09-07 18:04:31 +0530

    Add Keys Function

---
+++ js-drill-4/testPolyfill2.js
@@ -0,0 +1,6 @@
+const {testObject, keys} = require("./polyfill2")
+
+
+console.log("----------------------Keys Function----------------------")
+const keyOfObject = keys(testObject)
+console.log(keyOfObject)
