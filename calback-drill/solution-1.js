535de3de3c064b428ed6f7a406a0a357cfc1bcce
hamza1851 <hamza.dev011@gmail.com>
2024-09-23 12:44:34 +0530

    Add createDirectory Function

--- calback-drill/solution-1.js
+++ calback-drill/solution-1.js
@@ -1,58 +1,73 @@
-import fs from "fs"
-import path from "path"
-
 /*
-    Problem 1:
-    
-    Using callbacks and the fs module's asynchronous functions, do the following:
-        1. Create a directory of random JSON files
-        2. Delete those files simultaneously 
+Problem 1:
+
+Using callbacks and the fs module's asynchronous functions, do the following:
+1. Create a directory of random JSON files
+2. Delete those files simultaneously 
 */
 
-function createDirectory(directoryPath, directoryName, callback) {
+const { default: fs } = await import("fs/promises")
+const path = await import("path")
+
+const createDirectory = async (directoryPath, directoryName) => {
   const directoryNameAndPath = path.join(directoryPath, directoryName)
-
-  // Create the directory if it doesn't exist
-  fs.mkdir(directoryNameAndPath, { recursive: true }, (error) => {
-    if (error) {
-      return callback(error) // if error occurs
-    }
-    callback(null, `Directory created successfully: ${directoryNameAndPath}`)
-  })
-}
-
-function createMultipleFiles(directoryPath, numberOfFiles, callback) {
-  let completed = 0 // Counter to track completed file creations
-  const errors = [] // Store errors if any
-
-  for (let i = 1; i <= numberOfFiles; i++) {
-    const filenameAndPath = path.join(directoryPath, `random-file-${i}.json`)
-    const content = JSON.stringify({
-      message: `This is the file random-file-${i}`,
-    })
-
-    fs.writeFile(filenameAndPath, content, (err) => {
-      if (err) {
-        errors.push(err) // Collect error
-      }
-
-      completed++ // Increment the completed counter
-
-      if (completed === numberOfFiles) {
-        if (errors.length > 0) {
-          return callback(errors) // if errors occurs
-        }
-        callback(null, `All ${numberOfFiles} files created successfully.`)
-      }
-    })
+  // if the directory created successfully then return msg otherwise error
+  try {
+    await fs.mkdir(directoryNameAndPath)
+    return `Directory created successfully: ${directoryNameAndPath}`
+  } catch (error) {
+    throw new Error(error)
   }
 }
 
-function deleteDirectory(directoryPathAndName, callback) {
-  fs.rm(directoryPathAndName, { recursive: true, force: true }, (err) => {
-    if (err) return callback(err)
-    callback(null, `Directory ${directoryPathAndName} deleted successfully`)
-  })
-}
+export { createDirectory }
 
-export { createDirectory, createMultipleFiles, deleteDirectory }
+// import fs from "fs"
+// import path from "path"
+// function createDirectory(directoryPath, directoryName, callback) {
+//   const directoryNameAndPath = path.join(directoryPath, directoryName)
+
+//   // Create the directory if it doesn't exist
+// fs.mkdir(directoryNameAndPath, { recursive: true }, (error) => {
+//   if (error) {
+//     return callback(error) // if error occurs
+//   }
+//   callback(null, `Directory created successfully: ${directoryNameAndPath}`)
+// })
+// }
+
+// function createMultipleFiles(directoryPath, numberOfFiles, callback) {
+//   let completed = 0 // Counter to track completed file creations
+//   const errors = [] // Store errors if any
+
+//   for (let i = 1; i <= numberOfFiles; i++) {
+//     const filenameAndPath = path.join(directoryPath, `random-file-${i}.json`)
+//     const content = JSON.stringify({
+//       message: `This is the file random-file-${i}`,
+//     })
+
+//     fs.writeFile(filenameAndPath, content, (err) => {
+//       if (err) {
+//         errors.push(err) // Collect error
+//       }
+
+//       completed++ // Increment the completed counter
+
+//       if (completed === numberOfFiles) {
+//         if (errors.length > 0) {
+//           return callback(errors) // if errors occurs
+//         }
+//         callback(null, `All ${numberOfFiles} files created successfully.`)
+//       }
+//     })
+//   }
+// }
+
+// function deleteDirectory(directoryPathAndName, callback) {
+// fs.rm(directoryPathAndName, { recursive: true, force: true }, (err) => {
+//   if (err) return callback(err)
+//   callback(null, `Directory ${directoryPathAndName} deleted successfully`)
+// })
+// }
+
+// export { createDirectory, createMultipleFiles, deleteDirectory }
--- calback-drill/test/test-solution-1.js
+++ calback-drill/test/test-solution-1.js
@@ -1,22 +1,28 @@
-import {
-  createDirectory,
-  createMultipleFiles,
-  deleteDirectory,
-} from "../solution-1.js"
+import { createDirectory } from "../solution-1.js"
 
-createDirectory("../", "random-JSON-files", (err, msg) => {
-  if (err) return console.log(err)
-  console.log(msg)
+const testProblem1 = async () => {
+  try {
+    const dirCreationMsg = await createDirectory("../", "random-JSON-files")
+    console.log(dirCreationMsg)
+  } catch (error) {
+    console.log(error)
+  }
+}
+testProblem1()
 
-  return createMultipleFiles("../random-JSON-files", 5, (err, msg) => {
-    if (err) return console.log(err)
-    console.log(msg)
-    // using setTimeout to slow down the deletion code to make it easy to observe
-    return setTimeout(() => {
-      return deleteDirectory("../random-JSON-files", (err, msg) => {
-        if (err) return console.log(err)
-        console.log(msg)
-      })
-    }, 5000)
-  })
-})
+// createDirectory("../", "random-JSON-files", (err, msg) => {
+//   if (err) return console.log(err)
+//   console.log(msg)
+
+//   return createMultipleFiles("../random-JSON-files", 5, (err, msg) => {
+//     if (err) return console.log(err)
+//     console.log(msg)
+//     // using setTimeout to slow down the deletion code to make it easy to observe
+//     return setTimeout(() => {
+//       return deleteDirectory("../random-JSON-files", (err, msg) => {
+//         if (err) return console.log(err)
+//         console.log(msg)
+//       })
+//     }, 5000)
+//   })
+// })
