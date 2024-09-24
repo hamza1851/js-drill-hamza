72c8d1084c59c4e22cb5ccba493ebd87542a4db3
hamza1851 <hamza.dev011@gmail.com>
2024-09-24 16:35:15 +0530

    Add deleteDirectoryFiles function

--- js-drill-8/problem-1.js
+++ js-drill-8/problem-1.js
@@ -48,4 +48,51 @@ const createMultipleFiles = (directoryPath, numberOfFiles) => {
   })
 }
 
-export { createDirectory, createMultipleFiles }
+const deleteDirectoryFiles = (directoryPathAndName) => {
+  return new Promise((resolve, reject) => {
+    let deleted = 0
+    const errors = []
+
+    fs.readdir(
+      directoryPathAndName,
+      { encoding: "utf-8" },
+      (error, filenames) => {
+        //taking errors to prior check and reject
+        if (error) {
+          reject(`Error reading directory: ${error.message}`)
+        } else {
+          //storing promises in map array to track in future through Promise.all
+          const fileDeletionPromises = filenames.map((filename) => {
+            return new Promise((res, rej) => {
+              const filePath = path.join(directoryPathAndName, filename)
+              fs.unlink(filePath, (error) => {
+                if (error) {
+                  errors.push(error)
+                  rej(error)
+                } else {
+                  deleted++
+                  res()
+                }
+              })
+            })
+          })
+
+          // Wait for all file deletions to complete
+          Promise.all(fileDeletionPromises)
+            .then(() => {
+              if (errors.length > 0) {
+                reject(`Errors occurred during file deletion: ${errors}`)
+              } else {
+                resolve(`All ${deleted} files have been deleted successfully.`)
+              }
+            })
+            .catch((error) => {
+              reject(`Error occurred during deletion: ${error}`)
+            })
+        }
+      }
+    )
+  })
+}
+
+export { createDirectory, createMultipleFiles, deleteDirectoryFiles }
--- js-drill-8/test/test-problem-1.js
+++ js-drill-8/test/test-problem-1.js
@@ -1,9 +1,17 @@
-import { createDirectory, createMultipleFiles } from "../problem-1.js"
+import {
+  createDirectory,
+  createMultipleFiles,
+  deleteDirectoryFiles,
+} from "../problem-1.js"
 
 createDirectory("../", "random-JSON-files")
   .then((msg) => {
     console.log(msg)
     return createMultipleFiles("../random-JSON-files", 5)
+  })
+  .then((msg) => {
+    console.log(msg)
+    return deleteDirectoryFiles("../random-JSON-files")
   })
   .then((msg) => {
     console.log(msg)
