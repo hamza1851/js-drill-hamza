ae0a15a67d28f326dabdcced537c6652f961bf12
hamza1851 <hamza.dev011@gmail.com>
2024-09-26 11:42:38 +0530

    Add structure

---
+++ async-await-drill/problem-1.js
@@ -0,0 +1,7 @@
+/*
+Problem 1:
+
+Using callbacks and the fs module's asynchronous functions, do the following:
+1. Create a directory of random JSON files
+2. Delete those files simultaneously 
+*/
